6b5aaf7d6e49c56f5f469010fe48a1e3c3fb9e0f
wawa3113 <sherlynanazwamutiya@gmail.com>
2025-11-08 17:01:39 +0700

    Tambah file koneksi database di branch KoneksiDatabase

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<h1>File pertama</h1>
+<?php
+// Koneksi ke database MySQL
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "db_mahasiswa";
+
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+} else {
+    echo "Koneksi ke database berhasil!";
+}
+?>
